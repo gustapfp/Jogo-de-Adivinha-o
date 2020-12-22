26350036dd250b155ec1fe77e28130c76f868a74
Gustavo Figueiredo <gustavopfp.trab@gmail.com>
2020-12-22 17:38:32 -0300

    adicionado verificação de letras da forca

--- forca.py
+++ forca.py
@@ -14,31 +14,65 @@ def jogar():
     aleatorio = ["abelha", "canguru", "girafa", "leopardo", "leão", "macaco", "lontra", "panda", "urso polar", "rinoceronte", "tigre", "tartaruga", "morsa", "zebra", "Abacaxi", "Açaí", "Banana", "Maça", "Laranja", "Pera", "Morango", "uva", "cocô", "Televisão", "Fogão" ,"Geladeira", "Mesa",  "cadeira", "Armários", "Microondas", "Batedeira", "Liquidificador"]
     
     
-    def oculta_palavra(palavra): 
+    def oculta_palavra(palavra): # oculta a palavra sorteada para o jogador
         tamanho_da_palavra = int(len(palavra))
-        user_advinha_palavra = ""
+        letras = ["_" for i in range(tamanho_da_palavra)]
 
-        for letra in range(tamanho_da_palavra):
-            user_advinha_palavra += "_ "
-        return user_advinha_palavra
-    
+        return letras
 
 
-
-    def escolhe_palavra(tema_do_usuario):
+    def escolhe_palavra(tema_do_usuario): # sorteia uma palavra para o jogador
         if tema_do_usuario == 1: 
-            palavra_escolhida = animais[randint(0, len(animais))]
+            palavra_escolhida = animais[randint(0, len(animais)-1)]
             return palavra_escolhida, oculta_palavra(palavra_escolhida)
         elif tema_do_usuario == 2:
-            palavra_escolhida = frutas[randint(0, len(frutas))]
+            palavra_escolhida = frutas[randint(0, len(frutas)-1)]
             return palavra_escolhida, oculta_palavra(palavra_escolhida)      
         elif tema_do_usuario == 3:
-            palavra_escolhida = objetos_de_casa[randint(0, len(objetos_de_casa))]
+            palavra_escolhida = objetos_de_casa[randint(0, len(objetos_de_casa)-1)]
             return palavra_escolhida, oculta_palavra(palavra_escolhida)
         elif tema_do_usuario == 4:
-            palavra_escolhida = aleatorio[randint(0, len(aleatorio))]
+            palavra_escolhida = aleatorio[randint(0, len(aleatorio)-1)]
             return palavra_escolhida, oculta_palavra(palavra_escolhida)
-    palavra_escolhida, palavra_escondida_para_usuario = escolhe_palavra(escolhe_tema)
+
+    # def check_palavra_secreta(palavra): # checa se todas as letras escondidas ja foram encontradas
+    #     if "_" in palavra:
+    #         return True
+    #     elif rodadas == 0:
+    #         return False
+    #     elif  "_" not in letras_ocultas:
+    #         return False
+
+    palavra_escolhida, letras_ocultas = escolhe_palavra(escolhe_tema) 
+    tamanho_da_palavra = int(len(palavra_escolhida))
+    # palavra_escolhida é a palavra sorteada para o úsuario
+    # palavras ocultas é a palavra que será mostra para o usuario em forma de _
     
 
-    
+
+    rodadas = 6
+    print(palavra_escolhida)
+
+    x = True
+    while x == True:         
+        if rodadas <= 0:
+            x = False
+            print(letras_ocultas)
+        elif  "_" not in letras_ocultas:
+            x = False
+            print(letras_ocultas)
+        else:
+            print(letras_ocultas)
+            chuta_letra = input('Escolhe uma letra:')
+            for i in range(tamanho_da_palavra):
+                if palavra_escolhida[i] == chuta_letra:
+                    letras_ocultas[i] = chuta_letra
+        
+
+
+
+
+        #check_palavra_secreta(letras_ocultas)
+
+        
+        
