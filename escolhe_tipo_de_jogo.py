46687d7c67d9b9f170f6a428e33cad510cb8e153
Gustavo Figueiredo <gustavopfp.trab@gmail.com>
2020-12-22 12:13:22 -0300

    adicionado arquivo foca .py

--- escolhe_tipo_de_jogo.py
+++ escolhe_tipo_de_jogo.py
@@ -1,9 +1,11 @@
 import advinhacao
 import advinhacao_com_dicas
+import forca
 
 print("Olá! Seja bem vindo ao jogo de advinhação. Escolha um modo de jogo:")
 print("Advinhação com dicas(1)")
 print("Advinhação sem dicas(2)")
+print("Forca (3)")
 
 jogo_escolhido = int(input("Jogo escolhido:"))
 
@@ -11,3 +13,5 @@ if jogo_escolhido == 1:
     advinhacao_com_dicas.jogar()
 elif jogo_escolhido == 2:
     advinhacao.jogar()
+elif jogo_escolhido == 3:
+    forca.jogar()
