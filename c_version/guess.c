c48c314785d4af63d364dfd897184186fb83b8ac
Gustavo Figueiredo <gustavopfp.trab@gmail.com>
2021-06-21 23:06:31 -0300

    Number of chances add

--- c_version/guess.c
+++ c_version/guess.c
@@ -10,26 +10,59 @@ int main() {
     printf("The secret number is: %d\n", secret_number);
 
     int guess; 
-    printf("Guess the secret number:\n");
-    scanf("%d", &guess); // %d to int's
-    printf("Your guess is: %d and the secret number was: %d\n ", guess, secret_number);
-    if (guess == secret_number) {
+    int right_answer = (guess == secret_number);
+    int high_right_answer = (guess < secret_number);
+   
+    
+    // and the secret number was: %d\n ", guess, secret_number);
+    /* while loop version:
+    int counter = 0;
+    while (counter < 3)
+    {
+        printf("Guess the secret number:\n");
+        scanf("%d", &guess); // %d to int's
+        printf("Your guessed: %d \n ", guess);
+
+        if (right_answer) {
         printf("Congratulations, you hit the secret number!\n");
         printf("Try again!\n");
+        }
+        else if(high_right_answer) {
+            printf("What a drag, you choose a number higher than the secret number!\n"); //  The Correct Number was:%d \n", secret_number);
+            printf("Try again!\n");
+        }
+        else
+        {
+            printf("What a drag, you choose a number lesser than the secret number!\n"); //  The Correct Number was:%d \n", secret_number);
+            printf("Try again!\n");
+        }
+        counter = counter + 1;
+        
     }
-    else if(guess > secret_number) {
-        printf("What a bag you choose a number higher than the secret number!\n"); //  The Correct Number was:%d \n", secret_number);
-        printf("Try again!\n");
-    }
-    else if (guess < secret_number)
+    */
+   // for version
+
+   for (int i=1; i<=3; i++)
     {
-        printf("What a bag you choose a number lesser than the secret number!\n"); //  The Correct Number was:%d \n", secret_number);
+        printf("Guess the secret number:\n");
+        scanf("%d", &guess); // %d to int's
+        printf("Your guessed: %d \n ", guess);
+
+        if (right_answer) {
+        printf("Congratulations, you hit the secret number!\n");
         printf("Try again!\n");
+        }
+        else if(high_right_answer) {
+            printf("What a drag, you choose a number higher than the secret number!\n");
+            printf("Try again!\n");
+        }
+        else
+        {
+            printf("What a drag, you choose a number lesser than the secret number!\n"); 
+            printf("Try again!\n");
+        }
+      
     }
-    
-    
-    
-    
     
 }
     
