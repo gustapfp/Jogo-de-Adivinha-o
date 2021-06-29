e8699d4531956f739a301389c733ccbb317d1116
Gustavo Figueiredo <gustavopfp.trab@gmail.com>
2021-06-29 14:59:56 -0300

    underlines

--- c_version/hangman.c
+++ c_version/hangman.c
@@ -3,25 +3,47 @@
 int main() {
 
     char secret_word[20];
-    sprintf(secret_word, "gustavo");
+    sprintf(secret_word, "banana");
+    
     // printf("%s", secret_word);
     
     int hanged = 0;
     int right_answer = 0;
-    do {
-        char guess;
-        printf("Choose a lether\n");
-        scanf("%c", &guess);
-        for (int i = 0; i < strlen(secret_word); i++) {
-            if (secret_word[i] == guess) {
-                printf("you suggested a right letter! You chosse the lether %c there was in the %d in the secret world. :o \n", guess, i);
-            }
-        }
+    
+    char word_guessed[26];
+    int rounds = 0;
 
-    } while (!hanged && !right_answer);
+    do {
+
+        for (int i = 0; i < strlen(secret_word); i++) {
+            int finded = 0;
+
+            for (int j=0; j < rounds; j++) {
+                if (word_guessed[j] == secret_word[i]) {
+                    finded = 1;
+                    break;
+                }
+            }
+
+            if (finded) {
+                printf("%c ", secret_word[i]);
+            } else {
+                printf("_ ");
+                }          
+            }
+        printf("\n");
+        
+        char guess;
+        printf("Choose a lether:");
+        scanf(" %c", &guess);
+
+        word_guessed[rounds] = guess;
+        rounds++;
+        }while (!hanged && !right_answer);
     
     
     /*
+    printf("You suggested a right letter! The lether %c are in the %d position of the secret world. :o \n", guess, i+1);
     secret_word[0] = "g";
     secret_word[1] = "u";
     secret_word[2] = "s";
