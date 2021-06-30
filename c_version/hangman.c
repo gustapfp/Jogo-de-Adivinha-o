73507d6ad870a5e3fc31e9484ce093314ace1486
Gustavo Figueiredo <gustavopfp.trab@gmail.com>
2021-06-29 21:14:45 -0300

    code refactoring

--- c_version/hangman.c
+++ c_version/hangman.c
@@ -1,70 +1,82 @@
 #include <stdio.h>
 #include <string.h>
 
+char secret_word[20];
+char word_guessed[26];
+int rounds = 0;
+
 void greetings() {
     printf("**************************************\n");
     printf("  *  Welcome to the hangman game!  *  \n");
     printf("**************************************\n");
 }
 
-void new_guess(char word[26], int* round) {
+void new_guess() {
     char guess;
     printf("Choose a lether:");
     scanf(" %c", &guess);
-    word[*round] = guess;
-    (*round)++;
+
+    word_guessed[rounds] = guess;
+    rounds++;
 }
+
+int check_guess(char letter) {
+    int finded = 0;
+    for (int j=0; j < rounds; j++) {
+        if (word_guessed[j] == letter) {
+            finded = 1;
+            break;
+        }
+    } 
+    return finded; 
+} 
+
+void hangman() {
+    for (int i = 0; i < strlen(secret_word); i++) {           
+        int finded = check_guess(secret_word[i]);
+        if (finded) {
+            printf("%c ", secret_word[i]);
+        } else {
+            printf("_ ");
+        }      
+    }
+}
+
+void choose_secret_word() {
+    sprintf(secret_word, "banana");
+}
+
+int hanged() {
+    int cont_wrong_guesses = 0; 
+
+    for(int i = 0; i < rounds; i++) {
+        int right_guess = 0;
+
+        for(int j = 0; j < strlen(secret_word); j++) {
+
+            if (word_guessed[i] == secret_word[j]) {
+                right_guess = 1;
+                break;
+            } 
+            
+        }
+        if (!right_guess) cont_wrong_guesses++;         
+    }
+    return  cont_wrong_guesses >= 5;
+}
+
 int main() {
 
-    greetings();
-
-    char secret_word[20];
-    sprintf(secret_word, "banana");
-    
-    // printf("%s", secret_word);
-    
-    int hanged = 0;
     int right_answer = 0;
     
-    char word_guessed[26];
-    int rounds = 0;
+    choose_secret_word();
+    greetings();
 
-    do {
-
-        for (int i = 0; i < strlen(secret_word); i++) {
-            int finded = 0;
-
-            for (int j=0; j < rounds; j++) {
-                if (word_guessed[j] == secret_word[i]) {
-                    finded = 1;
-                    break;
-                }
-            }
-
-            if (finded) {
-                printf("%c ", secret_word[i]);
-            } else {
-                printf("_ ");
-                }          
-            }
+    
+    do { 
+        hangman();
         printf("\n");
-        new_guess(word_guessed, &rounds);
-        }while (!hanged && !right_answer);
-    
-    
-    /*
-    printf("You suggested a right letter! The lether %c are in the %d position of the secret world. :o \n", guess, i+1);
-    secret_word[0] = "g";
-    secret_word[1] = "u";
-    secret_word[2] = "s";
-    secret_word[3] = "t";
-    secret_word[4] = "a";
-    secret_word[5] = "v";
-    secret_word[6] = "o";
-    secret_word[7] = "\0";
-
-    */
-
-    
+        new_guess(word_guessed);
+        } while (!hanged() && !right_answer);  
     
 }
